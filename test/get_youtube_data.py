6ddab1894ad3ca1b340f2a35a4d24f5d8d9af734
akihiro.ito <gyouza.0822.ossan@gmail.com>
2023-06-07 11:53:57 +0900

    upload requirement and sample file

---
+++ test/get_youtube_data.py
@@ -0,0 +1,22 @@
+from apiclient.discovery import build
+import yaml
+
+with open('./key.yaml', 'r') as yml:
+    key_dict = yaml.safe_load(yml)
+
+videoId = '6RLhVKmwraM'
+YOUTUBE_API_KEY = key_dict['api_key']
+
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+videos_response = youtube.videos().list(
+    part='snippet,statistics',
+    id='{},'.format(videoId)
+).execute()
+# snippet
+snippetInfo = videos_response["items"][0]["snippet"]
+# 動画タイトル
+title = snippetInfo['title']
+# チャンネル名
+channeltitle = snippetInfo['channelTitle']
+print(channeltitle)
+print(title)
